121af7766adeff6605f521cf5c7b5a51c1d28f9a
Javier Vergara <ja.vergarac96@gmail.com>
2024-05-10 17:06:42 -0400

    Funciones de Consulta

--- querys/query.js
+++ querys/query.js
@@ -1,5 +1,6 @@
 const pool = require ('../config/configDB');
 
+// Muestra todos los usuarios de la tabla
 async function usuarios() {
     const queryJson = {
         text: 'SELECT * FROM usuarios'
@@ -10,6 +11,18 @@ async function usuarios() {
     return rows;
 }
 
+// Muestra todos los usuarios con el campo activo en TRUE
+async function usuariosActivos() {
+    const queryJson = {
+        text: 'SELECT * FROM usuarios WHERE activa = TRUE'
+    };
+
+    const { rows } = await pool.query(queryJson);
+
+    return rows;
+}
+
+// Crea un nuevo registro de usuario con nombre y balance
 async function nuevoUsuario(nombre, balance) {
     const queryJson = {
         text: `INSERT INTO usuarios (nombre, balance) VALUES ($1, $2) RETURNING *`,
@@ -21,6 +34,7 @@ async function nuevoUsuario(nombre, balance) {
     return results;
 }
 
+// Consulta todos los datos de un usuario
 async function consultarUsuario(id) {
     const queryJson = {
         text: 'SELECT * FROM usuarios WHERE id = $1',
@@ -32,6 +46,7 @@ async function consultarUsuario(id) {
     return rows;
 }
 
+// Editar campos nombre y balance de un usuario en la tabla
 async function editarUsuario(id, nombre, balance) {
     const queryJson = {
         text: `UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *`,
@@ -43,6 +58,7 @@ async function editarUsuario(id, nombre, balance) {
     return rows;
 }
 
+// Elimina el usuario de la tabla
 async function eliminarUsuario(id) {
     const queryJson = {
         text: `DELETE FROM usuarios WHERE id = $1 RETURNING *`,
@@ -54,6 +70,19 @@ async function eliminarUsuario(id) {
     return rows;
 }
 
+// Realiza una actualizacion en el campo activo de la tabla usuarios para desactivarlo (FALSE)
+async function desactivarUsuario(id) {
+    const queryJson = {
+        text: `UPDATE usuarios SET activa = FALSE WHERE id = $1 RETURNING *`,
+        values: [id]
+    }
+
+    const { rows } = await pool.query(queryJson);
+
+    return rows;
+}
+
+// Realiza una nueva transferencia entre 2 usuarios, emisor y receptor
 async function nuevaTransferencia(emisor, receptor, monto) {
     await pool.query("BEGIN");
 
@@ -69,6 +98,7 @@ async function nuevaTransferencia(emisor, receptor, monto) {
     await pool.query("COMMIT");
 }
 
+// Muestra todas las transferencias realizadas
 async function historialTransferencias() {
     const queryJson = {
         text: 'SELECT t.fecha, u_emisor.nombre AS emisor, u_receptor.nombre AS receptor, t.monto FROM transferencias t INNER JOIN usuarios u_emisor ON t.emisor = u_emisor.id JOIN usuarios u_receptor ON t.receptor = u_receptor.id',
@@ -80,6 +110,7 @@ async function historialTransferencias() {
     return rows;
 }
 
+// Consulta si usuario cuenta con saldo disponible
 async function getSaldoEmisor(id){
     const queryJson = {
         text: 'SELECT balance from usuarios WHERE id = $1',
@@ -91,6 +122,18 @@ async function getSaldoEmisor(id){
     return resultado.rows[0].balance;
 }
 
+// Consulta si usuario existe en la tabla transferencias
+async function usuarioTransferencia(id){
+    const queryJson = {
+        text: 'SELECT * from transferencias WHERE emisor = $1 OR receptor = $1',
+        values: [id]
+    }
+
+    const resultado = await pool.query(queryJson);
+    
+    return resultado;
+}
+
 module.exports = {
     usuarios,
     nuevoUsuario,
@@ -99,5 +142,8 @@ module.exports = {
     eliminarUsuario,
     nuevaTransferencia,
     historialTransferencias,
-    getSaldoEmisor
+    getSaldoEmisor,
+    desactivarUsuario,
+    usuariosActivos,
+    usuarioTransferencia
 }
